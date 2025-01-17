0c8792febdbcf4656da858d181b46bc2a0cfde69
Curtis J Pierce <Curtisjpierce39@gmail.com>
2025-01-17 12:48:46 -0800

    add initial description

---
+++ mastering-react-functional-components/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Main } from './components/Main';
+import './components/Styles.css'
+import {Routes, Route} from 'react-router-dom'
+import { Marvel } from "./components/CharacterDetails";
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/'element={<Main />} />
+        <Route path='/:id' element={<Marvel />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App;
---
+++ mastering-react-functional-components/src/components/CharacterDetails.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+export const Marvel = () => {
+    const {id}=useParams();
+    const [item,setItems]=useState()
+    const fetch=async()=>{
+        const res=await axios.get(`https://gateway.marvel.com:443/v1/public/characters/${id}?ts=1&apikey=d0137fa40d5132a1d5e0eb148caa0932&hash=b4b87f94c2284ac8230ef8908d164ee8`)
+        setItems(res.data.data.results[0])
+    }
+    fetch();
+    return (
+        <>
+        {
+            (!item)? "":(
+                <div className="box-content">
+                    <div className="right-box">
+                        <img src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt="hero image" />
+                    </div>
+                    <div className="left-box">
+                        <h1>{item.name}</h1>
+                        <h4>{item.description}</h4>
+                        <h4>{`${item.comics.items}`}</h4>
+                    </div>
+                </div>
+            )
+        }
+        </>
+    )
+}
